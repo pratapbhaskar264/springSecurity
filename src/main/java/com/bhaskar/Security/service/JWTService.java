e27cf2a1ef716654a55a83921c79672e20124bd6
pratabhaskar264 <pratapbhaskar264@gmail.com>
2025-08-22 21:38:56 +0530

    updated JWTService

--- src/main/java/com/bhaskar/Security/service/JWTService.java
+++ src/main/java/com/bhaskar/Security/service/JWTService.java
@@ -1,23 +1,20 @@
 package com.bhaskar.Security.service;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-
-import javax.crypto.SecretKey;
-
-
-
-import javax.crypto.SecretKey;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 
 @Service
 public class JWTService {
